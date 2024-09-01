efc2c5058ab0b90656f19da0844197a50bd03eec
Anne Liza Wangui <annewangui210@gmail.com>
2024-09-01 18:19:21 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    if length == 1:
+        print([0])
+        return
+    
+    if length == 2:
+        print([0,1])
+        return
+    
+    a, b = 0, 1
+    fibonacci_sequence = [a]
+
+    while b <= length:
+       fibonacci_sequence.append(b)
+       a, b = b, a + b
+    
+    print (fibonacci_sequence)
+
+print_fibonacci(34)
